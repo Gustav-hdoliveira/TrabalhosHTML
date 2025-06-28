d93edd0d89b6a3f20c586501ddd36db4fefa8e6d
GUSTAVO HENRIQUE DE OLIVEIRA <gustavbu9@gmail.com>
2025-06-27 21:33:16 -0300

    10atc PUSH

---
+++ ConectJS/script.js
@@ -0,0 +1,8 @@
+let nome = ("Gustavo Henrique")
+let curso = ("Programação")
+
+function boasVindas() {
+    return `Bem vindo, ${nome}! Você está no curso de ${curso}.`
+}
+
+console.log(boasVindas())
